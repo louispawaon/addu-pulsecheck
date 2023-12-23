c3fb46cfd441c2a61699014156dd142ba069243d
louispawaon <lpawaon@gmail.com>
2023-12-23 15:24:39 +0800

    chore: initialized shadcn ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,87 +1,80 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss"
 
-const config: Config = {
+const config = {
+  darkMode: ["class"],
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+	],
+  prefix: "",
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
       },
     },
-    colors: {
-      'text': {
-        50: 'var(--text-50)',
-        100: 'var(--text-100)',
-        200: 'var(--text-200)',
-        300: 'var(--text-300)',
-        400: 'var(--text-400)',
-        500: 'var(--text-500)',
-        600: 'var(--text-600)',
-        700: 'var(--text-700)',
-        800: 'var(--text-800)',
-        900: 'var(--text-900)',
-        950: 'var(--text-950)',
+    extend: {
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
       },
-      'background': {
-        50: 'var(--background-50)',
-        100: 'var(--background-100)',
-        200: 'var(--background-200)',
-        300: 'var(--background-300)',
-        400: 'var(--background-400)',
-        500: 'var(--background-500)',
-        600: 'var(--background-600)',
-        700: 'var(--background-700)',
-        800: 'var(--background-800)',
-        900: 'var(--background-900)',
-        950: 'var(--background-950)',
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
       },
-      'primary': {
-        50: 'var(--primary-50)',
-        100: 'var(--primary-100)',
-        200: 'var(--primary-200)',
-        300: 'var(--primary-300)',
-        400: 'var(--primary-400)',
-        500: 'var(--primary-500)',
-        600: 'var(--primary-600)',
-        700: 'var(--primary-700)',
-        800: 'var(--primary-800)',
-        900: 'var(--primary-900)',
-        950: 'var(--primary-950)',
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
       },
-      'secondary': {
-        50: 'var(--secondary-50)',
-        100: 'var(--secondary-100)',
-        200: 'var(--secondary-200)',
-        300: 'var(--secondary-300)',
-        400: 'var(--secondary-400)',
-        500: 'var(--secondary-500)',
-        600: 'var(--secondary-600)',
-        700: 'var(--secondary-700)',
-        800: 'var(--secondary-800)',
-        900: 'var(--secondary-900)',
-        950: 'var(--secondary-950)',
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
-      'accent': {
-        50: 'var(--accent-50)',
-        100: 'var(--accent-100)',
-        200: 'var(--accent-200)',
-        300: 'var(--accent-300)',
-        400: 'var(--accent-400)',
-        500: 'var(--accent-500)',
-        600: 'var(--accent-600)',
-        700: 'var(--accent-700)',
-        800: 'var(--accent-800)',
-        900: 'var(--accent-900)',
-        950: 'var(--accent-950)',
-      },
-     },     
+    },
   },
-  plugins: [],
-}
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config
+
 export default config
