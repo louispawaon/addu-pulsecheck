a38fa230bf5309a9d6c1b63d3ec8e099b228ce6a
louispawaon <lpawaon@gmail.com>
2024-01-04 10:04:03 +0800

    refactor(websitecardstory): added status

--- src/stories/ui/WebsiteCard.stories.tsx
+++ src/stories/ui/WebsiteCard.stories.tsx
@@ -10,5 +10,5 @@ export default meta;
 type Story = StoryObj<typeof WebsiteCard>;
 
 export const Primary: Story = {
-  render: () => <WebsiteCard proxyUrl='' websiteDescription='' websiteName='' websiteUrl=''/>,
+  render: () => <WebsiteCard proxyUrl='' websiteDescription='' websiteName='' websiteUrl='' status=''/>,
 };
