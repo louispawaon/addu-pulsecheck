3587c25421bf10f7abfb50f60a8910ec0bd8135d
louispawaon <lpawaon@gmail.com>
2024-01-07 18:24:35 +0800

    refactor(websitecard): design changes

--- src/app/components/ui/WebsiteCard.tsx
+++ src/app/components/ui/WebsiteCard.tsx
@@ -8,6 +8,9 @@ import {
     CardHeader,
     CardTitle,
 } from "@/components/ui/card"
+import StatusBadge from '@/app/components/ui/StatusBadge'
+import Link from 'next/link'
+import { nunito_sans, poppins } from '@/app/styles/fonts'
 
 type WebsiteCardProps = WebsiteType & {
     status: string;
@@ -16,16 +19,14 @@ type WebsiteCardProps = WebsiteType & {
 const WebsiteCard: React.FC<WebsiteCardProps> = ({websiteName, websiteDescription, websiteUrl, websiteThumbnail, status}) => {
 
     return (
-        <Card>
-        <CardHeader>
-            <CardTitle>{websiteName}</CardTitle>
-            <CardDescription>{websiteDescription}</CardDescription>
+        <Card className={`${poppins.variable} bg-primary text-foreground shadow-lg`}>
+        <CardHeader className='p-3'>
+            <CardTitle  className='font-medium md:text-3xl sm:text-xl'>{websiteName}</CardTitle>
+            <CardDescription className={`${nunito_sans.variable} sm:justify-start text-primary-foreground`}>{websiteDescription}</CardDescription>
+            <Link href={websiteUrl} className='hover:underline sm:text-xs md:text-lg italic'>{websiteUrl}</Link>
         </CardHeader>
-        <CardContent>
-            <p>{websiteUrl}</p>
-        </CardContent>
-        <CardFooter>
-            <p>Status: {status}</p>
+        <CardFooter className='p-2'>
+            <StatusBadge status={status}/>
         </CardFooter>
         </Card>
       )
