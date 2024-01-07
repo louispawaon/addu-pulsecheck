8294ba15dbeb38c2dacedacbf459eec000c4d2eb
louispawaon <lpawaon@gmail.com>
2024-01-07 18:23:27 +0800

    refactor(navbar): changed design - missing logo - will include soon

--- src/app/components/ui/Navbar.tsx
+++ src/app/components/ui/Navbar.tsx
@@ -1,9 +1,16 @@
+import Link from 'next/link';
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
+    <nav className="bg-primary p-4">
+      <div className="container mx-auto flex items-center justify-center">
+        <Link className="text-white text-2xl font-bold" href="/">
+          PulseCheck
+        </Link>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar
